6bfb3c072f7546a7cc0518be59d60e7a55a08fd6
ygohappy123 <hagiahuy21052003@gmail.com>
2025-10-14 20:28:23 +0700

    refactor code

--- src/configs/corsOptions.ts
+++ src/configs/corsOptions.ts
@@ -1,8 +1,10 @@
+import { CorsOptions } from 'cors'
 import { HttpException } from '@/errors/HttpException'
-import allowedOrigins from '@/configs/allowedOrigins'
 import errorMessage from '@/configs/errorMessage'
 
-const corsOptions = {
+const allowedOrigins = ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:4000', 'http://127.0.0.1:4000']
+
+const corsOptions: CorsOptions = {
     origin: (origin: string | undefined, callback: (error: Error | null, allow?: boolean) => void) => {
         if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true)
@@ -10,6 +12,7 @@ const corsOptions = {
             callback(new HttpException(403, errorMessage.BLOCKED_BY_CORS_POLICY))
         }
     },
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
     optionsSuccessStatus: 200
 }
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
+import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import multer from 'multer'
-import dotenv from 'dotenv'
-dotenv.config()
 
 import * as routes from '@/routes'
 import { parsedEnv } from '@/env'
