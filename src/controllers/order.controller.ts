dec5ce8c99ee875220f73afe7e49768ff81c66a2
ygohappy123 <hagiahuy21052003@gmail.com>
2025-12-20 10:50:56 +0700

    change AI configs

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -47,48 +47,6 @@ const orderController = {
         }
     },
 
-    getCustomerOrders: async (req: RequestWithAuthData, res: Response, next: NextFunction) => {
-        try {
-            const { userId } = req.auth!
-            const { skip, limit, sort, filter } = req.query
-            const { orders, total } = await orderService.getCustomerOrders(
-                {
-                    skip: skip !== undefined ? parseInt(skip as string) : undefined,
-                    limit: limit !== undefined ? parseInt(limit as string) : undefined,
-                    sort,
-                    filter
-                } as ISearchParams,
-                userId
-            )
-
-            res.status(200).json({
-                data: orders,
-                total,
-                took: orders.length
-            })
-        } catch (error) {
-            next(error)
-        }
-    },
-
-    placeNewOrder: async (req: RequestWithAuthData, res: Response, next: NextFunction) => {
-        try {
-            const errors = validationResult(req)
-            if (!errors.isEmpty()) throw new HttpException(422, errorMessage.DATA_VALIDATION_FAILED)
-
-            const { userId } = req.auth!
-            const { note, coupon, recipientName, deliveryAddress, deliveryPhone, items } = req.body
-            const result = await orderService.placeNewOrder(note, coupon, recipientName, deliveryAddress, deliveryPhone, items, userId)
-
-            res.status(201).json({
-                data: { orderId: result.orderId },
-                message: successMessage.PLACE_ORDER_SUCCESSFULLY
-            })
-        } catch (error) {
-            next(error)
-        }
-    },
-
     processOrder: async (req: RequestWithAuthData, res: Response, next: NextFunction) => {
         try {
             const errors = validationResult(req)
--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -7,8 +7,6 @@ const router = express.Router()
 
 router.post('/verify-coupon', customerOnly, verifyCouponValidator, orderController.verifyCoupon)
 router.get('/', staffOnly, orderController.getAllOrders)
-router.get('/my-orders', customerOnly, orderController.getCustomerOrders)
-router.post('/', customerOnly, placeNewOrderValidator, orderController.placeNewOrder)
 router.patch('/:orderId/status', staffOnly, processOrderValidator, orderController.processOrder)
 
 export default router
